ed62ae7fc59c91ef9f957d7de28b14adb844ac03
mistad <soraxmistad@hotmail.com>
2013-06-21 02:23:36 -0400

    Added Modified chemlights, A3 Chems lasted too long.

--- Client/dayz_code/cfgAmmo.hpp
+++ Client/dayz_code/cfgAmmo.hpp
@@ -10,4 +10,28 @@ class CfgAmmo {
 		hit = 9;
 		simulation = "shotBullet";
 	};
+	class Chemlight_green;
+	class Chemlight_blue;
+	class Chemlight_yellow;
+	class Chemlight_red;
+	
+	class Chemlight_green_DZ: Chemlight_green
+	{
+		timeToLive = 300;
+	};
+	
+	class Chemlight_yellow_DZ: Chemlight_yellow
+	{
+		timeToLive = 240;
+	};
+	
+	class Chemlight_red_DZ: Chemlight_red
+	{
+		timeToLive = 360;
+	};
+	
+	class Chemlight_blue_DZ: Chemlight_blue
+	{
+		timeToLive = 360;
+	};
 };
