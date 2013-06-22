660365b74b0ddf294875d8ce5d01955b7e36c30a
Pwnoz0r <pwnoz0r@gmail.com>
2013-06-22 13:04:31 -0400

    0.1.4.3

--- Client/dayz_code/config.cpp
+++ Client/dayz_code/config.cpp
@@ -41,7 +41,7 @@ class CfgMods
 		hidePicture = 0;
 		hideName = 0;
 		action = "http://www.zoombiesmod.com";
-		version = "0.1.4.2";
+		version = "0.1.4.3";
 		hiveVersion = 0.96; //0.93
 	};
 	class DZ_InitWorld
